c885d1e50749a8d0b4f3d562dfb69a18077351c0
Nicole Ketchiozo <nicolek@mac.myfiosgateway.com>
2025-10-12 23:39:33 -0400

    added blueprint add-ons

--- main-eks.tf
+++ main-eks.tf
@@ -4,6 +4,20 @@ provider "aws" {
   #secret_key = var.aws_secret_access_key
 }
 
+provider "helm" {
+  kubernetes = {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
+}
+
+
 # VPC for Cluster
 data "aws_availability_zones" "azs" {}
 
@@ -52,11 +66,11 @@ module "eks" {
   aws_auth_roles            = local.aws_k8s_role_mapping
 
 
-  cluster_addons = {
-    kube-proxy = {}
-    vpc-cni    = {}
-    coredns    = {}
-  }
+  #cluster_addons = {
+  #  kube-proxy = {}
+  #  vpc-cni    = {}
+  #  coredns    = {}
+  #}
 
   eks_managed_node_groups = {
     initial = {
@@ -69,3 +83,34 @@ module "eks" {
 
   tags = var.tags
 }
+
+module "eks_blueprints_addons" {
+  source  = "aws-ia/eks-blueprints-addons/aws"
+  version = "~> 1.0" #ensure to update this to the latest/desired version
+
+  cluster_name      = module.eks.cluster_name
+  cluster_endpoint  = module.eks.cluster_endpoint
+  cluster_version   = module.eks.cluster_version
+  oidc_provider_arn = module.eks.oidc_provider_arn
+
+  eks_addons = {
+    aws-ebs-csi-driver = {
+      most_recent = true
+    }
+    coredns = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+  }
+
+  enable_aws_load_balancer_controller = true
+  enable_metrics_server               = true
+  enable_cluster_autoscaler           = true
+
+}
+
