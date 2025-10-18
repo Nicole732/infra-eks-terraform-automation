c1b7780ccaf10bb3ee12b1c41e3f8c3c2b294b8d
Nicole Ketchiozo <nicolek@Nicoles-MacBook-Pro.local>
2025-10-18 13:28:37 -0400

    troubleshooting versions

--- providers.tf
+++ providers.tf
@@ -8,12 +8,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0, < 5.0" 
+      version = "~> 5.0" #">= 4.0, < 5.0" 
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.38.0"
+      version = "~> 2.29.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"       # 2.11–2.13 are steady with EKS
     }
 
 
